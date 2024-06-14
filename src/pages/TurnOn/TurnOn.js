50a41a8fa7ce2c658a0e6f9a216123484eaebf4f
Lucas Eneas <lucaseneasmarco@gmail.com>
2024-06-14 13:22:56 -0300

    insert useNavigate

--- src/pages/TurnOn/TurnOn.js
+++ src/pages/TurnOn/TurnOn.js
@@ -1,22 +1,26 @@
 
+import { useNavigate } from 'react-router-dom';
 import './TurnOn.css'
 
-function lightTurnOn() {
-  const button = document.querySelector('#turnOnBackground')
-  button.classList.add('turnOnBackgroundAnimation')
-  setTimeout(() => {
-    window.location.href = './home';
-  }, 3000);
-}
+
 
 function TurnOn() {
+  const navigate = useNavigate();
+
+  function lightTurnOn() {
+    const button = document.querySelector('#turnOnBackground')
+    button.classList.add('turnOnBackgroundAnimation')
+    setTimeout(() => {
+      navigate('./home');
+    }, 3000);
+  }
+
   return (
     <section id='turnOnBackground' className='turnOnBackground'>
       <button onClick={lightTurnOn} id='turnOnButton' className='turnOnButton'>
         <label>
           <input type="checkbox" /><i></i>
         </label>
-
       </button>
     </section>
 
